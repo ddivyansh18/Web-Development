b6c1791510566989447fb911c0b7aa16b751bb33
DIVYANSH DIXIT <ddivyansh18@gmail.com>
2020-10-13 16:00:01 +0530

    Add files via upload

---
+++ js/iframeHandler.js
@@ -0,0 +1,4 @@
+function displayIframe() {
+        document.getElementById("iframeDisplay").innerHTML = "<iframe src="https://i.simmer.io/@ddivyansh18/sample-cylinder" style="width:960px;height:600px;border:0"></iframe>";
+
+    }
